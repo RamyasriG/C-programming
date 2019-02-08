fd31ce6ac04328539835feafd625978d9b05cc98
RamyasriG <47448972+RamyasriG@users.noreply.github.com>
2019-02-08 17:01:35 +0530

    Create positivenegative.c

---
+++ positivenegative.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+ 
+int main() {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("\nPositive");
+	}
+	else if(n<0)
+   {
+	printf("\nNegative");
+   }
+   else
+  {
+	printf("\nZero");
+  }
+	return 0;
+}
